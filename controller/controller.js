e61f56c2f61a56c28f7f67d4acb77196c316c887
Jonathan <jicaicedor@unl.edu.ec>
2018-03-10 22:42:33 -0500

    Ultimo commit

--- controller/controller.js
+++ controller/controller.js
@@ -1,22 +1,22 @@
-var app = angular.module('app', ['ngRoute','ngResource']);
+var app = angular.module('app', []);
 
 
 app.controller('ctrlUsuario', function($scope, $http, $location, $window){
 
-	
-	$scope.getParameterByName=function(name) {
+  
+  $scope.getParameterByName=function(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
     results = regex.exec(location.search);
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
- 	};
+  };
 
-  	var usuario = $scope.getParameterByName('us');
+    var usuario = $scope.getParameterByName('us');
   
 
 
- 	$scope.Cargar=function(){
-  		
+  $scope.Cargar=function(){
+      
      $http({
         method: 'GET',
         url: '../controller/edit_us.php',
@@ -31,57 +31,58 @@ app.controller('ctrlUsuario', function($scope, $http, $location, $window){
       },function errorCallback(datosDependencias)
       {
       
+
         console.log("Error, al tratar de traer los datos")
       }); 
    
     };
 
-	$scope.Cargar();
+  $scope.Cargar();
 
-  	$scope.edit= function (user){
-  		$scope.user=user;
-		if(confirm("esta seguro de guardar")){
-			 
-			 $http({
-			 	method: 'GET',
-			 	url: '../model/editUser.php',
-			 	params: {usuario:$scope.user.nombre, 
-			 		apellido:$scope.user.apellido, 
-			 		correo:$scope.user.correo}
-			 })
-			.then(function successCallback(datosDependencias)
-      		{
-      			
-        		$scope.names = datosDependencias.data.records;
-        		console.log(datosDependencias);
+    $scope.edit= function (user){
+      $scope.user=user;
+    if(confirm("esta seguro de guardar")){
+       
+       $http({
+        method: 'GET',
+        url: '../model/editUser.php',
+        params: {usuario:$scope.user.nombre, 
+          apellido:$scope.user.apellido, 
+          correo:$scope.user.correo}
+       })
+      .then(function successCallback(datosDependencias)
+          {
+            
+            $scope.names = datosDependencias.data.records;
+            console.log(datosDependencias);
 
-     		},function errorCallback(datosDependencias)
-      		{
-        		
-        		console.log("Error, al tratar de traer los datos")
-      		});
+        },function errorCallback(datosDependencias)
+          {
+            
+            console.log("Error, al tratar de traer los datos")
+          });
 
             $window.location.href='user.php';
-		}
-	};
-		
+    }
+  };
+    
 
-	
+  
 
-	
-	$scope.getParameterByName=function(name) {
+  
+  $scope.getParameterByName=function(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
     results = regex.exec(location.search);
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
- 	};
+  };
 
-  	var usuario = $scope.getParameterByName('us');
+    var usuario = $scope.getParameterByName('us');
   
 
 
- 	$scope.Cargar=function(){
-  		
+  $scope.Cargar=function(){
+      
      $http({
         method: 'GET',
         url: '../controller/edit_us.php',
@@ -95,90 +96,111 @@ app.controller('ctrlUsuario', function($scope, $http, $location, $window){
 
       },function errorCallback(datosDependencias)
       {
-        alert(datosDependencias);
+    
         console.log("Error, al tratar de traer los datos")
       }); 
    
     };
 
-	$scope.Cargar();
+  $scope.Cargar();
 
-  	$scope.edit= function (user){
-  		$scope.user=user;
-		if(confirm("esta seguro de guardar")){
-			 alert($scope.user.nombre);
-			 $http({
-			 	method: 'GET',
-			 	url: '../model/editUser.php',
-			 	params: {usuario:$scope.user.nombre, 
-			 		apellido:$scope.user.apellido, 
-			 		correo:$scope.user.correo}
-			 })
-			.then(function successCallback(datosDependencias)
-      		{
-      			alert(datosDependencias);
-        		$scope.names = datosDependencias.data.records;
-        		console.log(datosDependencias);
+    $scope.edit= function (user){
+      
+      $scope.user=user;
+    //  $scope.iinput = document.getElementById('inputFileServer');
+    //if($scope.iinput.files && $scope.iinput.files[0])
+      // console.log("File Seleccionado : ", $scope.iinput.files[0]);
+      //if(typeof($scope.iinput.files[0])=='undefined')
+    //{
+    //  $scope.iinput.files=$scope.img;
+      
+    //}
+    
+    if(confirm("esta seguro de guardar")){
+       $http({
+        method: 'GET',
+        url: '../model/editUser.php',
+        params: {usuario: $scope.user.nombre, 
+          apellido: $scope.user.apellido, 
+          correo: $scope.user.correo}
+          /*imagen:$scope.iinput.files[0].name*/
+       })
+      .then(function successCallback(datosDependencias)
+          {
+            
+            $scope.names = datosDependencias.data.records;
+            console.log(datosDependencias);
 
-     		},function errorCallback(datosDependencias)
-      		{
-        		alert(datosDependencias);
-        		console.log("Error, al tratar de traer los datos")
-      		});
+        },function errorCallback(datosDependencias)
+          {
+            console.log(datosDependencias);
+            console.log("Error, al tratar de traer los datos")
+          });
 
             $window.location.href='user.php';
-		}
-	};
-		
+    }
+  };
+    
 
-	
-
-
-	$scope.cancel= function(){
-		$window.location.href='user.php';
-	}
-
-});
-app.controller('crtlBusqueda',function($scope, $http){
-	$scope.categorias=[
-	{
-		idcat:1,
-		nombre:"Terror"
-	},
-	{
-		idcat:2,
-		nombre:"Literatura"
-	},
-	{
-		idcat:3,
-		nombre:"Filosofia"
-	},
-	{
-		idcat:4,
-		nombre:"Novela"
-	}
-	];
-	
- alert($scope.categorias);
+  //nsnns
 
 
-
-app.controller('crtlBusqueda', function($scope, $http){
-	$scope.categorias = [
-	{ id : "1", nombre : "Terror" },
-	{ id : "2", nombre : "Literatura" },
-	{ id : "3", nombre : "Filosofia" },
-	{ id : "4", nombre : "Novela" }
-	];
-	
-
+  $scope.cancel= function(){
+    $window.location.href='user.php';
+  }
 
 });
-app.controller('myCtrl', function($scope) {
-    $scope.cars = [
-        {model : "Ford Mustang", color : "red"},
-        {model : "Fiat 500", color : "white"},
-        {model : "Volvo XC90", color : "black"}
-    ];
 
-});
+app.controller('crtlBusqueda', ['$scope','$http', function($scope, $http){
+  $scope.ban=false;
+  $scope.cate = [
+  { id : "1", nombre : "Poesia" },
+  { id : "2", nombre : "Filosofia" },
+  { id : "3", nombre : "Novelas" },
+  { id : "4", nombre : "Literatura" }
+  ];
+
+  $scope.buscar=function(){
+
+
+    
+    $scope.libro="f";
+    //alert($scope.libro.categoria);
+    if(typeof($scope.nombre)=='undefined')
+    {
+      $scope.nombre="n";
+      
+    }
+    if(typeof($scope.autor)==='undefined')
+    {
+      $scope.autor="n";
+    }
+    if(typeof($scope.categoria)==='undefined')
+    {
+      $scope.categoria="null";
+    } 
+    
+    $http({
+      method: 'GET',
+      url: '../model/busqulibro.php',
+      params: {nombre: $scope.nombre,
+          autor: $scope.autor,
+          categoria: $scope.categoria}
+    })
+    .then(function successCallback(datosDependencias)
+    {
+      $scope.nombre="";
+      $scope.autor="";
+      $scope.categoria="";
+      $scope.librosrec= datosDependencias.data.records;
+      console.log(datosDependencias);
+
+      $scope.ban=true;
+
+
+    },function errorCallback(datosDependencias)
+    {
+      console.log(datosDependencias);
+    });
+
+  };
